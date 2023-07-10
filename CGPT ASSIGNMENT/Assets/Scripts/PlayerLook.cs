1a61f2d43fc6702215cc9e1a6eb2dbdb1dea24c8
Diviyan20 <diviyanr@gmail.com>
2023-07-10 19:01:44 +0800

    Basic First Person Movement
    Player can look around and also move in first person

---
+++ CGPT ASSIGNMENT/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSens = 500f;
+    public Transform Body;
+
+    float xRotation = 0f;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+
+        Body.Rotate(Vector3.up * mouseX); 
+    }
+
+
+
+}

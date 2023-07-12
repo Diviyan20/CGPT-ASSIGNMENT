ce22984c234249cbad6d65748b57c839b1d948c4
AhChia <wtchia2003@gmail.com>
2023-07-12 11:54:53 +0800

    Timer

---
+++ CGPT ASSIGNMENT/Assets/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+    }
+    // Update is called once per frame
+    void Update () {
+    }
+
+    public void Restart()
+	{
+        //restart the level named gameScene
+        SceneManager.LoadScene ("gameScene");
+    }
+
+    public void Exit()
+	{
+        //quit the game
+        Application.Quit ();
+    }
+} 
+
---
+++ CGPT ASSIGNMENT/Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 90;
+    public TextMeshProUGUI timerText;
+
+    void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>(); // Assign the TextMeshProUGUI component
+    }
+
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+			SceneManager.LoadScene("gameOverScene");
+        }
+
+        if (timeValue < 0)
+        {
+            timeValue = 0;
+        }
+        float minutes = Mathf.FloorToInt(timeValue / 60);
+        float seconds = Mathf.FloorToInt(timeValue % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}

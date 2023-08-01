2cb0ae006348dc60fcc58d489e1ccae571001cc1
AhChia <wtchia2003@gmail.com>
2023-08-01 21:50:47 +0800

    Volume and brightness sync in different scenes

--- CGPT ASSIGNMENT/Assets/Scripts/Brightness.cs
+++ CGPT ASSIGNMENT/Assets/Scripts/Brightness.cs
@@ -6,29 +6,49 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class Brightness : MonoBehaviour
 {
-	public Slider brightnessSlider;
-	public PostProcessProfile brightness;
-	public PostProcessLayer layer;
-	
-	AutoExposure exposure;
-	
-    // Start is called before the first frame update
-    void Start()
+    public Slider brightnessSlider;
+    public PostProcessProfile brightness;
+    public PostProcessLayer layer;
+
+    AutoExposure exposure;
+
+    private void Start()
     {
         brightness.TryGetSettings(out exposure);
-		AdjustBrightness(brightnessSlider.value);
+        LoadBrightnessValue(); // Load the brightness value when the scene starts
     }
-	
-	public void AdjustBrightness(float value)
-	{
-		if(value != 0)
-		{
-			exposure.keyValue.value = value;
-		}
-		else
-		{
-			exposure.keyValue.value = .05f;
-		}
-	}
 
+    public void AdjustBrightness(float value)
+    {
+        if (value != 0)
+        {
+            exposure.keyValue.value = value;
+        }
+        else
+        {
+            exposure.keyValue.value = .05f;
+        }
+
+        SaveBrightnessValue(); // Save the brightness value when adjusted
+    }
+
+    private void LoadBrightnessValue()
+    {
+        if (PlayerPrefs.HasKey("BrightnessValue"))
+        {
+            float savedValue = PlayerPrefs.GetFloat("BrightnessValue");
+            brightnessSlider.value = savedValue;
+            AdjustBrightness(savedValue);
+        }
+        else
+        {
+            brightnessSlider.value = 0.5f; // Set default value if not found
+            AdjustBrightness(0.5f);
+        }
+    }
+
+    private void SaveBrightnessValue()
+    {
+        PlayerPrefs.SetFloat("BrightnessValue", brightnessSlider.value);
+    }
 }
--- CGPT ASSIGNMENT/Assets/Scripts/VolumeManager.cs
+++ CGPT ASSIGNMENT/Assets/Scripts/VolumeManager.cs
@@ -2,13 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
-	public AudioMixer audioMixer;
-	
-	public void SetVolume (float volume)
-	{
-		audioMixer.SetFloat("volume",volume);
-	}
+    public AudioMixer audioMixer;
+    public Slider volumeSlider; // Reference to the UI Slider
+
+    private void Start()
+    {
+        LoadVolumeValue(); // Load the volume value when the scene starts
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+        SaveVolumeValue(); // Save the volume value when adjusted
+    }
+
+    private void LoadVolumeValue()
+    {
+        if (PlayerPrefs.HasKey("VolumeValue"))
+        {
+            float savedValue = PlayerPrefs.GetFloat("VolumeValue");
+            volumeSlider.value = savedValue;
+            SetVolume(savedValue);
+        }
+        else
+        {
+            volumeSlider.value = 0.5f; // Set default value if not found
+            SetVolume(0.5f);
+        }
+    }
+
+    private void SaveVolumeValue()
+    {
+        PlayerPrefs.SetFloat("VolumeValue", volumeSlider.value);
+    }
 }

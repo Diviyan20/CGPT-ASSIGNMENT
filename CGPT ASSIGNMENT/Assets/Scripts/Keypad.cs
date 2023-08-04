0a57aee26436066be29c556200bc7da050c8c8ca
Diviyan20 <diviyanr@gmail.com>
2023-08-04 09:13:32 +0800

    Fixed: Timer stops when player enters correct password

--- CGPT ASSIGNMENT/Assets/Scripts/Keypad.cs
+++ CGPT ASSIGNMENT/Assets/Scripts/Keypad.cs
@@ -8,8 +8,9 @@ public class Keypad : MonoBehaviour
     public Text DisplayNumbers; // Referring to the number display Text UI element
     private string enteredNumbers = ""; // Storing the entered numbers as a string
     private int passwordLength = 4; // Setting the max password Length to 4 numbers
-    private string password = "6712";
+    private string password = "6716";
     public GameObject door; // Reference to the door GameObject
+    public TimeController timer;
 
     // Method to handle button presses and update the number display
     public void OnButtonPress(string buttonValue)
@@ -40,6 +41,7 @@ public class Keypad : MonoBehaviour
             DisplayNumbers.text = "CORRECT!";
             // Disable the door GameObject when the correct password is entered
             door.SetActive(false);
+            timer.StopTimer();
         }
         else
         {

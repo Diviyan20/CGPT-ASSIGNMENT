17cc80c0815385d12931b0f44b7285280ada8e94
AhChia <wtchia2003@gmail.com>
2023-09-13 17:55:56 +0800

    Remove useless script

--- CGPT ASSIGNMENT/Assets/Scripts/MainMenuController.cs
+++ CGPT ASSIGNMENT/Assets/Scripts/MainMenuController.cs
@@ -10,41 +10,34 @@ public class MainMenuController : MonoBehaviour
 	public GameObject ToHide;
 	public GameObject ToShow;
 	
-    // Use this for initialization
     void Start ()
 	{
-		// Check if a checkpoint has been reached in any scene
         bool hasCheckpoint = PlayerPrefs.HasKey("CheckpointReached");
 
-        // Activate the "Continue" button if a checkpoint has been reached
         continueButton.gameObject.SetActive(hasCheckpoint);
 		
 		ToShow.SetActive(false);
         Cursor.visible = true;
     }
-    // Update is called once per frame
+	
     void Update ()
 	{
     }
 	
 	public void ContinueGame()
     {
-        // Load the scene where the player left off
         string lastScene = PlayerPrefs.GetString("LastScene");
         SceneManager.LoadScene(lastScene);
     }
 	
 	public void StartNewGame()
     {
-        // Set the checkpoint key to indicate a checkpoint has been reached
         PlayerPrefs.SetString("CheckpointReached", "true");
 
-        // Load the first level or your starting scene
         SceneManager.LoadScene("ROOM 1");
     }
 	
     public void Settings(){
-        //restart the level named gameScene
         ToShow.SetActive(true);
         ToHide.SetActive(false);
     }
@@ -57,7 +50,6 @@ public class MainMenuController : MonoBehaviour
 
     public void Exit()
 	{
-        //quit the game
         Application.Quit();
     }
 } 
